2f094891a41b70b3cb381e9c6838a39cf4f73f5d
Nikita Barskov <nbarskov@gmail.com>
2025-10-23 00:56:34 +0200

    Update `install.sh`

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 ln -s --force ~/personal/dotfiles/.gitconfig ~/.gitconfig
 ln -s --force ~/personal/dotfiles/hirn.studio.gitconfig ~/.config/git/hirn.studio.gitconfig
-ln -s --force ~/personal/dotfiles/personal.gitconfig ~/.config/git/personal.gitconfig
+ln -s --force ~/personal/dotfiles/nikitabarskov.gitconfig ~/.config/git/nikitabarskov.gitconfig
 ln -s --force ~/personal/dotfiles/ignore ~/.config/git/ignore
