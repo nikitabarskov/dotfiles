74ffda3ee425dcf40f58897c0dc96f8e25ecca6f
Nikita Barskov <nbarskov@gmail.com>
2025-11-02 17:22:02 +0100

    Install aerospace

--- install.sh
+++ install.sh
@@ -11,20 +11,21 @@ ln -fsv $(pwd)/nikitabarskov.gitconfig ${XDG_CONFIG_HOME}/git/nikitabarskov.gitc
 ln -fsv $(pwd)/ignore ${XDG_CONFIG_HOME}/git/ignore
 ln -fsv $(pwd)/.aliases ${HOME}/.aliases
 ln -fsv $(pwd)/.zshrc ${HOME}/.zshrc
-[ -e "${XDG_CONFIG_HOME}/zed" ] && rm -f "${XDG_CONFIG_HOME}/zed"
+[ -e "${XDG_CONFIG_HOME}/zed" ] && rm -rf "${XDG_CONFIG_HOME}/zed"
 ln -fsv $(pwd)/.config/zed/settings.json ${XDG_CONFIG_HOME}/zed/settings.json
+[ ! -e "${XDG_CONFIG_HOME}/alacritty" ] && mkdir -p "${XDG_CONFIG_HOME}/alacritty"
 ln -fsv $(pwd)/.config/alacritty/alacritty.toml ${XDG_CONFIG_HOME}/alacritty/alacritty.toml
-[ -e "${XDG_CONFIG_HOME}/ghostty" ] && rm -f "${XDG_CONFIG_HOME}/ghostty"
+[ -e "${XDG_CONFIG_HOME}/ghostty" ] && rm -rf "${XDG_CONFIG_HOME}/ghostty"
 ln -fsv "$(pwd)/.config/ghostty" "${XDG_CONFIG_HOME}/ghostty"
-[ -e "${XDG_CONFIG_HOME}/gh" ] && rm -f "${XDG_CONFIG_HOME}/gh"
+[ -e "${XDG_CONFIG_HOME}/gh" ] && rm -rf "${XDG_CONFIG_HOME}/gh"
 ln -fsv "$(pwd)/.config/gh" "${XDG_CONFIG_HOME}/gh"
 ln -v -s --force $(pwd)/.profile ${HOME}/.profile
 
 if [[ $OSTYPE == "linux-gnu"* ]]; then
     echo "Detected Linux - configuring i3"
-    [ -L "${XDG_CONFIG_HOME}/i3" ] && rm -f "${XDG_CONFIG_HOME}/i3"
+    [ -L "${XDG_CONFIG_HOME}/i3" ] && rm -rf "${XDG_CONFIG_HOME}/i3"
     ln -fsv "$(pwd)/.config/i3" "${XDG_CONFIG_HOME}/i3"
-    [ -L "${XDG_CONFIG_HOME}/i3status-rust" ] && rm -f "${XDG_CONFIG_HOME}/i3status-rust"
+    [ -L "${XDG_CONFIG_HOME}/i3status-rust" ] && rm -rf "${XDG_CONFIG_HOME}/i3status-rust"
     ln -fsv "$(pwd)/.config/i3status-rust" "${XDG_CONFIG_HOME}/i3status-rust"
     [ ! -e "${XDG_CONFIG_HOME}/homebrew" ] && mkdir -p "${XDG_CONFIG_HOME}/homebrew"
     ln -fsv "${dir}/linux/Brewfile" "${XDG_CONFIG_HOME}/homebrew/Brewfile"
@@ -32,6 +33,9 @@ fi
 
 # Configure 1Password SSH signing path based on OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Detected macOS - configure Aerospace"
+    [ ! -e "${XDG_CONFIG_HOME}/aerospace" ] && mkdir -p "${XDG_CONFIG_HOME}/aerospace"
+    ln -fsv "${dir}/.config/aerospace/aerospace.toml" "${XDG_CONFIG_HOME}/aerospace/aerospace.toml"
     echo "Detected macOS - configuring 1Password SSH signing path"
     [ -d "/opt/1Password" ] && sudo mkdir -p /opt/1Password && sudo chown $(id -u):$(id -g) /opt/1Password
     [ ! -e "/opt/1Password/op-ssh-sign" ] && ln -fsv "/Applications/1Password.app/Contents/MacOS/op-ssh-sign" "/opt/1Password/op-ssh-sign"
