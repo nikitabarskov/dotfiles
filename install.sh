4242ff313ef7a8e5a7e0f85d7d1aa397310c26a8
Nikita Barskov <nbarskov@gmail.com>
2025-11-02 02:33:58 +0100

    Update `.alacritty.toml` and `install.sh`

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ ln -v -s --force $(pwd)/ignore ${HOME}/.config/git/ignore
 ln -v -s --force $(pwd)/.aliases ${HOME}/.aliases
 ln -v -s --force $(pwd)/.zshrc ${HOME}/.zshrc
 ln -v -s --force $(pwd)/.config/zed/settings.json ${HOME}/.config/zed/settings.json
+ln -v -s --force $(pwd)/.alacritty.toml ${HOME}/.config/alacritty/.alacritty.toml
+
+if [[ $OSTYPE == "linux-gnu"* ]]; then
+    echo "Detected Linux - configuring i3"
+    ln -v -s --force "$(pwd)/.config/i3" "${HOME}/.config/i3"
+    ln -v -s --force "$(pwd)/.config/i3status-rust" "${HOME}/.config/i3status-rust"
+fi
 
 # Configure 1Password SSH signing path based on OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
