082cae5c56e0376e41df24671dc5997b2b50840d
Nikita Barskov <nbarskov@gmail.com>
2025-11-02 01:42:21 +0100

    Update `install.sh`

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
-ln -s --force ~/personal/dotfiles/.gitconfig ~/.gitconfig
-ln -s --force ~/personal/dotfiles/hirn.studio.gitconfig ~/.config/git/hirn.studio.gitconfig
-ln -s --force ~/personal/dotfiles/nikitabarskov.gitconfig ~/.config/git/nikitabarskov.gitconfig
-ln -s --force ~/personal/dotfiles/ignore ~/.config/git/ignore
-ln -s --force ~/personal/dotfiles/.aliases ~/.aliases
-ln -s --force ~/personal/dotfiles/.zshrc ~/.zshrc
+dir=$(pwd)
+
+echo "Installing dotfiles from ${dir} to ${HOME}"
+
+ln -v -s --force $(pwd)/.gitconfig ${HOME}/.gitconfig
+ln -v -s --force $(pwd)/hirn.studio.gitconfig ${HOME}/.config/git/hirn.studio.gitconfig
+ln -v -s --force $(pwd)/nikitabarskov.gitconfig ${HOME}/.config/git/nikitabarskov.gitconfig
+ln -v -s --force $(pwd)/ignore ${HOME}/.config/git/ignore
+ln -v -s --force $(pwd)/.aliases ${HOME}/.aliases
+ln -v -s --force $(pwd)/.zshrc ${HOME}/.zshrc
+ln -v -s --force $(pwd)/.config/zed/settings.json ${HOME}/.config/zed/settings.json
+
+# Configure 1Password SSH signing path based on OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Detected macOS - configuring 1Password SSH signing path"
+    ln -v -s --force "/Applications/1Password.app/Contents/MacOS/op-ssh-sign" "/opt/1Password/op-ssh-sign"
+fi
