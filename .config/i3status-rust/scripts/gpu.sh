4b80d1c71adbcc86169cacacc5403a76a7c81a13
Nikita Barskov <nbarskov@gmail.com>
2025-11-12 23:25:01 +0100

    Remove `i3status-rust`

--- .config/i3status-rust/scripts/gpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
-util=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
-echo " GPU: ${util}% ${temp}°C"
-
--- .config/i3status-rust/scripts/root-space.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-used=$(df -h / | awk 'NR==2 {print $3}')
-total=$(df -h / | awk 'NR==2 {print $2}')
-percent=$(df -h / | awk 'NR==2 {print $5}')
-echo " Root: ${used}/${total} (${percent})"
